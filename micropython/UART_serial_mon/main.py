0a20e5310d6d12a03e47f15880c9281c53d13ccb
Ali Tariq <ali.tariq@10xengineers.ai>
2022-12-12 02:06:30 +0500

    Updated UART file in micropython

--- micropython/UART_serial_mon/main.py
+++ micropython/UART_serial_mon/main.py
@@ -1,5 +1,5 @@
 from machine import UART
 
 uart=UART(0,115200)
-uart.init(115200, bits=8, parity=None, stop=1)
+
 uart.write('Welcome to ESP32. Congratulations, you have accessed my serial monitor')
