6afb185ac87f30cf71b433b20dbc51918d38487a
Ali Tariq <ali.tariq@10xengineers.ai>
2022-12-17 13:21:44 +0500

    Complete Blinky code added

--- C_bareMetal/blinky_bare_metal/main/main.c
+++ C_bareMetal/blinky_bare_metal/main/main.c
@@ -1,15 +1,26 @@
-// It applies logic 1 on GPIO pin 2
-#define GPIO_FUNC2_OUT_SEL_CFG_REG 	*(volatile unsigned long *) 0x3FF44538
+// It applies logic 1 on a GPIO pin 13 of Lilygo TTCALL ESP32 module
+#define GPIO_FUNC13_OUT_SEL_CFG_REG *(volatile unsigned long *) 0x3FF44564
 #define GPIO_ENABLE_REG				*(volatile unsigned long *) 0x3FF44020
-#define IO_MUX_GPIO0_REG			*(volatile unsigned long *) 0x3FF49044
+#define IO_MUX_MTCK_REG 			*(volatile unsigned long *) 0x3FF49038
 #define GPIO_OUT_REG				*(volatile unsigned long *) 0x3FF44004
 #define GPIO_OUT_W1TS_REG			*(volatile unsigned long *) 0x3FF44008
 #define GPIO_ENABLE_W1TS_REG		*(volatile unsigned long *) 0x3FF44024
+#define GPIO_OUT_W1TC_REG			*(volatile unsigned long *) 0x3FF4400C
+void delay(long long unsigned int iterations_squared);
 
 void app_main(void){
-	GPIO_FUNC2_OUT_SEL_CFG_REG=0x100;
-	GPIO_ENABLE_W1TS_REG |= 1<<2;
-	GPIO_ENABLE_REG |= 1<<2;
-	GPIO_OUT_W1TS_REG |=1<<2;
-	GPIO_OUT_REG|=1<<2;
+	GPIO_FUNC13_OUT_SEL_CFG_REG=0x100;
+	IO_MUX_MTCK_REG |= 1<<13;
+	GPIO_ENABLE_REG |= 1<<13;
+	while (1){
+		GPIO_OUT_REG |= 1<<13; //Turn on LED
+		delay(3000);
+		GPIO_OUT_REG &= ~(1<<13); //Turn off LED
+		delay(3000);
+	}
+}
+
+void delay(long long unsigned int iterations_squared){ //Conventional delay function
+	for (unsigned int i=0;i<iterations_squared;i++)
+		for (unsigned int j=0;j<iterations_squared;j++);
 }
